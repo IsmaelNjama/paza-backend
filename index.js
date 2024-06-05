0d08e38cb7c9021aca8276b32f6f19c7fccb9bca
IsmaelNjama <petermailsnjama@gmail.com>
2024-06-05 18:53:12 +0300

    Add dotenv

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
-const express = require('express');
+require("dotenv").config();
+const express = require("express");
 const app = express();
-const port = process.env.PORT || 5000
+const port = process.env.PORT || 5000;
 
 app.listen(port, () => {
-    console.log(`Server is running on port ${port}...`)
-})
+  console.log(`Server is running on port ${port}...`);
+});
