64c7cb6d704a13b23a18ca1a69e69b3247a5702b
IsmaelNjama <petermailsnjama@gmail.com>
2024-06-08 23:14:13 +0300

    feat: add signup schema and implement schema validation

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -1,7 +1,9 @@
 const express = require("express");
 const router = express.Router();
 const authController = require("../controllers/auth.controller");
+const validate = require("../utils/schemaValidate");
+const signupUserSchema = require("../schema/signupUsers.schema");
 
-router.post("/signup", authController.signupUser);
+router.post("/signup", validate(signupUserSchema), authController.signupUser);
 
 module.exports = router;
