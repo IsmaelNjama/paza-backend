eef00590c18cf864e65b6b53b5f69dd6326860cf
IsmaelNjama <petermailsnjama@gmail.com>
2025-01-28 20:23:33 +0200

    send message and job after job creation

--- controllers/jobs.controller.js
+++ controllers/jobs.controller.js
@@ -4,7 +4,7 @@ module.exports = {
   createJob: async (req, res, next) => {
     try {
       const job = await jobsServices.createJob(req.body);
-      res.status(201).send("Job created successfully");
+      res.status(201).send({ message: "Job created successfully", job });
     } catch (error) {
       next(error);
     }
